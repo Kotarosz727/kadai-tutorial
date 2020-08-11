2eae1067e69a9076d3be9051a37a811d3f885e1f
Kotarosz727 <kotarosz7272gmail.com>
2020-08-11 12:23:30 +0900

    use laravel ui for login/register

--- app/User.php
+++ app/User.php
@@ -2,12 +2,14 @@
 
 namespace App;
 
-use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Database\Eloquent\Model;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Illuminate\Foundation\Auth\AuthenticatesUsers;
 
 class User extends Authenticatable
 {
+
     use Notifiable;
 
     protected $primaryKey = 'user_id';
--- routes/web.php
+++ routes/web.php
@@ -2,17 +2,10 @@
 
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
 Route::get('/', function () {
-    return view('welcome');
+    return redirect('posts');
 });
+Route::resource('posts', 'PostController')->except('edit', 'update', 'destory');
+Auth::routes();
+Route::post('/comments', 'CommentController@store');
+
